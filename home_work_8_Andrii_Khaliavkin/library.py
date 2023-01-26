442978750130843139d0b5e3aecea7233ba85e77
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-01-26 21:35:39 +0100

    solution 1

--- home_work_8_Andrii_Khaliavkin/library.py
+++ home_work_8_Andrii_Khaliavkin/library.py
@@ -1,19 +1,19 @@
 # написати функцію, яка отримує ціле число і повертає слово "копійка" у вірній формі: 1 -- копійка, 2 -- копійки, 25 -- копійок
 
 
-def coins(user_input):
+def coins(number):
     """     """
     try:
-        user_input = int(user_input)
-        if user_input < 0:
+        number = int(number)
+        if number < 0:
             print("кількість копійок не може бути від'ємною")
         else:
-            if user_input % 10 == 1 and user_input != 11:
-                return f"{user_input} копійка"
-            elif user_input % 10 in (2, 3, 4) and user_input not in (12, 13, 14):
-                return f"{user_input} копійки"
+            if number % 10 == 1 and number != 11:
+                return f"{number} копійка"
+            elif number % 10 in (2, 3, 4) and number not in (12, 13, 14):
+                return f"{number} копійки"
             else:
-                return f"{user_input} копійок"
+                return f"{number} копійок"
     except ValueError:
         print("приймаються тільки цілі числа без спец. символів")
 
