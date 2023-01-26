413d34a9e8d05b120200e91e672b4d1b34389852
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-01-26 23:56:56 +0100

    solution 2

--- home_work_8_Andrii_Khaliavkin/library.py
+++ home_work_8_Andrii_Khaliavkin/library.py
@@ -1,28 +1,35 @@
 # написати функцію, яка отримує ціле число і повертає слово "копійка" у вірній формі: 1 -- копійка, 2 -- копійки, 25 -- копійок
 
 
-def coins(number):
+def coins(coins_quantity):
     """     """
     try:
-        number = int(number)
-        if number < 0:
+        coins_quantity = int(coins_quantity)
+        if coins_quantity < 0:
             print("кількість копійок не може бути від'ємною")
         else:
-            if number % 10 == 1 and number != 11:
-                return f"{number} копійка"
-            elif number % 10 in (2, 3, 4) and number not in (12, 13, 14):
-                return f"{number} копійки"
+            if coins_quantity % 10 == 1 and coins_quantity != 11:
+                return f"{coins_quantity} копійка"
+            elif coins_quantity % 10 in (2, 3, 4) and coins_quantity not in (12, 13, 14):
+                return f"{coins_quantity} копійки"
             else:
-                return f"{number} копійок"
+                return f"{coins_quantity} копійок"
     except ValueError:
         print("приймаються тільки цілі числа без спец. символів")
 
 
-
-
-
-
-
-print(coins(-1))
-print(coins(2))
-print(coins('d'))
+def banknotes(banknotes_quantity):
+    """     """
+    try:
+        banknotes_quantity = int(banknotes_quantity)
+        if banknotes_quantity < 0:
+            print("кількість гривень не може бути від'ємною")
+        else:
+            if banknotes_quantity % 10 == 1 and banknotes_quantity != 11:
+                return f"{banknotes_quantity} гривня"
+            elif banknotes_quantity % 10 in (2, 3, 4) and banknotes_quantity not in (12, 13, 14):
+                return f"{banknotes_quantity} гривні"
+            else:
+                return f"{banknotes_quantity} гривень"
+    except ValueError:
+        print("приймаються тільки цілі числа без спец. символів")
