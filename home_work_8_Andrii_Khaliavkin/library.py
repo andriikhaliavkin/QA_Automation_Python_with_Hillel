0ded29dad6c060f37235fa5ddb2ef317284d4099
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-01-28 23:33:58 +0100

    refactor with pytest

--- home_work_8_Andrii_Khaliavkin/library.py
+++ home_work_8_Andrii_Khaliavkin/library.py
@@ -1,11 +1,8 @@
 import math
 
 
-# написати функцію, яка отримує ціле число і повертає слово "копійка" у вірній формі: 1 -- копійка, 2 -- копійки, 25 -- копійок
 def coins(coins_quantity):
     """
-        This function takes a number as an input and returns a string with the correct grammatical form of the word "копійка".
-        If the input is not a positive integer, the function will return an error message.
         :param coins_quantity: the number of coins
         :type coins_quantity: int / float
         :return: string with the grammatically correct form of "копійка"
@@ -25,12 +22,8 @@ def coins(coins_quantity):
     except ValueError:
         return f"приймаються тільки числа"
 
-
-# написати функцію, яка отримує ціле число і повертає слово "гривня" у вірній формі: 1 -- гривня, 2 -- гривні, 25 -- гривень
 def banknotes(banknotes_quantity):
     """
-        This function takes a number as an input and returns a string with the correct grammatical form of the word "гривня".
-        If the input is not a positive integer, the function will return an error message.
         :param banknotes_quantity: the number of banknotes
         :type banknotes_quantity: int / float
         :return: string with the grammatically correct form of "гривня"
@@ -51,17 +44,8 @@ def banknotes(banknotes_quantity):
         return f"приймаються тільки числа"
 
 
-# написати функцію, яка приймає число і повертає список, в якому перший елемент - стрічка з цілою частиною числа + слово 'гривня'
-# у вірному відмінку, другий елемент - стрічка з мантісою (значення після коми), + слово 'копійка' у вірній формі. \
-# наприклад - 1 -- ['1 гривня', "0 копійок"]; 10.1 -- ['10 гривень', "10 копійок"]; 2.01 -- ['2 гривні', "1 копійка"].
-# в даній функції для створення форм слів гривня та копійка використати результат роботи функцій, описаних вище.
-# Зауваження - якщо буде передано число з дрібною частиною більш ніж 2 знаки, то вони не мають оброблятися
-# (логіку, що робити з 125.339 залишаю на вас - чи округляйте, чи відкидайте - і це рішення пропишіть в докстрінгі)
-
 def atm(money):
     """
-    This function takes a number as an input and returns a list with integer and fractional parts of it with correct
-    grammatical form of the words "гривня" and "копійка". Fractional parts are rounded to 2 decimals after coma.
     :param money: the number of money
     :type money: int / float
     :return: splitted number on its integer and fractional parts with correct grammatical form of the words "гривня" and "копійка"
@@ -80,36 +64,20 @@ def atm(money):
         return f"приймаються тільки числа"
 
 
-# напишіть функцію is_hot_today, яка отримує параметр температури (число, за замовчуванням 30), і в залежності від величини
-# повідомляє, чи сьогодні жарко, чи холодно (більше 25 - жарко, інакше холодно). перевірку на -155555555555 градусів
-# чи +555555555555 не проводимо, просто відштовхуємося від отриманого значення. подумайте, який тип даних має повертати функція
-
-
-def is_hot_today(temperature=30):
+def check_the_weather(temperature=30):
     """
-        This function takes in an optional parameter 'temperature' which is a number representing the temperature.
-        It checks if the temperature is greater than 25, if yes, returns "Hot", otherwise returns "Cold".
-        The default value for temperature is 30.
-        In case of invalid input (not a number) it will throw ValueError and print "приймаються тільки числа"
         :param temperature: number which represents a temperature
         :type temperature: int / float
         :return: whether condition cold or hot
         :rtype: str
     """
-    try:
-        temperature = int(temperature)
-        if temperature > 25:
-            return "Hot"
-        else:
-            return "Cold"
-    except ValueError:
-        # print("приймаються тільки числа")
-        return f"приймаються тільки числа"
+    temperature = int(temperature)
+    if temperature > 25:
+        return "Hot"
+    else:
+        return "Cold"
 
-# створіть функцію, яка отримує від користувача число і повертає його як число. проте!!! якщо користувач ввів не вірні дані,
-# які не можна конвертувати в число ("шість"), заставте користувача ввести валідні дані (цикли вам в допомогу).
-# результатом в будь-якому випадку має бути число. зауважте - функція має прийняти один не обовязковий стрічковий
-# аргумент - месседж, за замовчуванням - "Введіть число"
+
 
 def user_number_input():
     """
--- home_work_8_Andrii_Khaliavkin/test.py
+++ home_work_8_Andrii_Khaliavkin/test.py
@@ -1,44 +1,37 @@
 import library
-import unittest
+import pytest
 
 
-class TestAll(unittest.TestCase):
-    def test_is_hot_today(self):
-        assert library.is_hot_today(30) == "Hot"
-        assert library.is_hot_today(20) == "Cold"
-        assert library.is_hot_today(35) == "Hot"
-        assert library.is_hot_today(-5) == "Cold"
-        assert library.is_hot_today(25) == "Cold"
-        assert library.is_hot_today(26) == "Hot"
-        assert library.is_hot_today() == "Hot"  # test with default value
-        assert library.is_hot_today("not a number") == "приймаються тільки числа"
-
-    def test_atm(self):
-        assert library.atm(100) == ["100 гривень", "0 копійок"]
-        assert library.atm(100.50) == ["100 гривень", "50 копійок"]
-        assert library.atm(-100) == "вартість не може бути від'ємною"
-        assert library.atm("a hundred") == "приймаються тільки числа"
-
-    def test_banknotes(self):
-        assert library.banknotes(1) == "1 гривня"
-        assert library.banknotes(5) == "5 гривень"
-        assert library.banknotes(11) == "11 гривень"
-        assert library.banknotes(12) == "12 гривень"
-        assert library.banknotes(21) == "21 гривня"
-        assert library.banknotes(22) == "22 гривні"
-        assert library.banknotes(25) == "25 гривень"
-        assert library.banknotes(-1) == "вартість не може бути від'ємною"
-        assert library.banknotes("abc") == "приймаються тільки числа"
-
-    def test_coins(self):
-        assert library.coins(5) == "5 копійок"
-        assert library.coins(12) == "12 копійок"
-        assert library.coins(1) == "1 копійка"
-        assert library.coins(2) == "2 копійки"
-        assert library.coins(-5) == "вартість не може бути від'ємною"
-        assert library.coins(5.5) == "5 копійок"
-        assert library.coins("five") == "приймаються тільки числа"
+@pytest.mark.parametrize("test_input, expected", [(30, "Hot"), (20, "Cold"), (35, "Hot"), (-5, "Cold")])
+def test_check_the_weather(test_input, expected):
+    assert library.check_the_weather(test_input) == expected
 
 
-suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestAll)
-unittest.TextTestRunner().run(suite)
+@pytest.mark.parametrize("test_input, expected", [(100, ["100 гривень", "0 копійок"]), (100.50, ["100 гривень", "50 копійок"])])
+def test_atm(test_input, expected):
+    assert library.atm(test_input) == expected
+#
+
+#     def test_banknotes(self):
+#         assert library.banknotes(1) == "1 гривня"
+#         assert library.banknotes(5) == "5 гривень"
+#         assert library.banknotes(11) == "11 гривень"
+#         assert library.banknotes(12) == "12 гривень"
+#         assert library.banknotes(21) == "21 гривня"
+#         assert library.banknotes(22) == "22 гривні"
+#         assert library.banknotes(25) == "25 гривень"
+#         assert library.banknotes(-1) == "вартість не може бути від'ємною"
+#         assert library.banknotes("abc") == "приймаються тільки числа"
+#
+#     def test_coins(self):
+#         assert library.coins(5) == "5 копійок"
+#         assert library.coins(12) == "12 копійок"
+#         assert library.coins(1) == "1 копійка"
+#         assert library.coins(2) == "2 копійки"
+#         assert library.coins(-5) == "вартість не може бути від'ємною"
+#         assert library.coins(5.5) == "5 копійок"
+#         assert library.coins("five") == "приймаються тільки числа"
+#
+#
+# suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestAll)
+# unittest.TextTestRunner().run(suite)
