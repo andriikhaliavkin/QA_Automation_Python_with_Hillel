9f74a569814f49dc93ad6c5523ec8ee9f3707f6c
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-01-29 11:58:44 +0100

    requirements

--- home_work_9_Andrii_Khaliavkin/main.py
+++ home_work_9_Andrii_Khaliavkin/main.py
@@ -1,7 +1,7 @@
 import library
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     print(library.uppercase_words(['alfred', 'ben', 'william']))
     print(library.square_and_round([4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59]))
-    print(library.zip_lists_to_dict(['a', 'b', 'c', 'd', 'e'], [1,2,3,4,5]))
+    print(library.zip_lists_to_dict(['a', 'b', 'c', 'd', 'e'], [1, 2, 3, 4, 5]))
     print(library.filter_strings_by_length(["Jeff", "Alex", "Jonathan", "Richelle", "Anna"], 5))
