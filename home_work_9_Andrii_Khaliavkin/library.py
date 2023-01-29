6f9189acaea1a3812300486b687ed13f07ea79e3
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-01-29 11:30:39 +0100

    solution 3

--- home_work_9_Andrii_Khaliavkin/library.py
+++ home_work_9_Andrii_Khaliavkin/library.py
@@ -1,4 +1,3 @@
-
 def uppercase_words(words: list) -> list:
     return list(map(str.upper, words))
 
@@ -6,8 +5,17 @@ def uppercase_words(words: list) -> list:
 # uppercase_words = lambda words: list(map(str.upper, words))
 
 
-def square_and_round(numbers):
-    return list(map(lambda x: round(x**2, 3), numbers))
+def square_and_round(numbers: list) -> list:
+    return list(map(lambda x: round(x ** 2, 3), numbers))
+
 
 # square_and_round = lambda numbers: list(map(lambda x: round(x**2, 3), numbers))
+
+
+def zip_lists_to_dict(keys: list, values: list) -> dict:
+    return dict(zip(keys, values))
+
+
+# zip_lists_to_dict = lambda keys, values: dict(zip(keys, values))
+
 
