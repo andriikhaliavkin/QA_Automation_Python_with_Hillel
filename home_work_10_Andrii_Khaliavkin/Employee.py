bb50583b03cd763da8a6fa5de8ed0303f235f8d5
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-01-31 18:27:56 +0100

    class employee

---
+++ home_work_10_Andrii_Khaliavkin/Employee.py
@@ -0,0 +1,50 @@
+# Створіть клас з описом будь-якого працівника(Типу універсальний). Employee.
+# Намагайтесь максимально повно описати класи на предмет полів і методів.
+# Додайте анотації до всіх методів. До кожного метода обов'язково треба
+# додати док стрінгу з описом метода і анотації.
+
+
+
+class Employee:
+    """
+    Class representing an employee.
+
+    Attributes:
+        name (str): Name of the employee.
+        position (str): Position of the employee.
+        salary (float): Salary of the employee.
+        experience (int): Years of experience of the employee.
+    """
+
+    def __init__(self, name: str, position: str, salary: float, experience: int):
+        """
+        Initialize an Employee instance.
+
+        Args:
+            name (str): Name of the employee.
+            position (str): Position of the employee.
+            salary (float): Salary of the employee.
+            experience (int): Years of experience of the employee.
+        """
+        self.name = name
+        self.position = position
+        self.salary = salary
+        self.experience = experience
+
+    def get_info(self) -> str:
+        """
+        Return a string with information about the employee.
+
+        Returns:
+            str: A string with information about the employee.
+        """
+        return f"{self.name} is a {self.position} with {self.experience} years of experience and a salary of {self.salary}."
+
+    def increase_salary(self, percent: float):
+        """
+        Increase the salary of the employee by a certain percentage.
+
+        Args:
+            percent (float): The percentage by which the salary will be increased.
+        """
+        self.salary *= 1 + percent / 100
