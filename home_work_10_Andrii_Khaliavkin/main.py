ab76dd08cab939bebe406bc77660da6da3e6cb2b
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-01-31 18:46:09 +0100

    solution

--- home_work_10_Andrii_Khaliavkin/main.py
+++ home_work_10_Andrii_Khaliavkin/main.py
@@ -4,5 +4,5 @@ import Employee
 google = Company.Company("Google", "Technology", 1998, "Mountain View, California", 160_000_000_000.0, 120_000)
 print(google.get_info())
 
-John_Snow = Employee.Employee("John Snow", "Black Crown", 100500.0, 20)
-print(John_Snow.get_info())
+crown = Employee.Employee("John Snow", "Black Crown", 100_500.0, 20)
+print(crown.get_info())
--- home_work_10_Andrii_Khaliavkin/test.py
+++ home_work_10_Andrii_Khaliavkin/test.py
@@ -20,3 +20,13 @@ def test_add_employees():
     google = Company.Company("Google", "Technology", 1998, "Mountain View, California", 160_000_000_000, 120_000)
     google.add_employees(5000)
     assert google.employees == 125_000
+
+def test_employee_info():
+    e = Employee.Employee("John Snow", "Crown", 100_500, 10)
+    info = e.get_info()
+    assert info == "John Snow is a Crown with 10 years of experience and a salary of 100500."
+
+def test_increase_salary():
+    e = Employee.Employee("John Snow", "Crown", 100_500, 10)
+    e.increase_salary(10)
+    assert e.salary == 110_550.00000000001
