202ee48e73fff90435164ac5aedc1efae4191731
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-02-02 22:00:07 +0100

    new tests

--- home_work_10_Andrii_Khaliavkin/test.py
+++ home_work_10_Andrii_Khaliavkin/test.py
@@ -3,7 +3,6 @@ import Company
 import Employee
 
 
-
 def test_get_info():
     google = Company.Company("Google", "Technology", 1998, "Mountain View, California", 160_000_000_000.0, 120_000)
     expected_info = "Google is a Technology company founded in 1998. Its headquarter is located in Mountain View, California and it has an annual revenue of 160000000000.0 and 120000 employees."
@@ -21,12 +20,26 @@ def test_add_employees():
     google.add_employees(5000)
     assert google.employees == 125_000
 
+
 def test_employee_info():
     e = Employee.Employee("John Snow", "Crown", 100_500, 10)
     info = e.get_info()
     assert info == "John Snow is a Crown with 10 years of experience and a salary of 100500."
 
+
 def test_increase_salary():
     e = Employee.Employee("John Snow", "Crown", 100_500, 10)
     e.increase_salary(10)
     assert e.salary == 110_550.00000000001
+
+
+def test_increase_experience():
+    e = Employee.Employee("John Snow", "Crown", 100_500, 20)
+    e.increase_experience()
+    assert e.experience == 21
+
+
+def test_change_position():
+    e = Employee.Employee("John Snow", "Crown", 100_500, 20)
+    e.change_position("Lanister")
+    assert e.position == "Lanister"
