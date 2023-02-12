021a4ae07c58a23738514427b8abc87cf6964aa0
Khalandr <48185617+Khalandr@users.noreply.github.com>
2023-02-12 19:25:49 +0100

    tests

--- home_work_12_Andrii_Khaliavkin/test.py
+++ home_work_12_Andrii_Khaliavkin/test.py
@@ -1,1 +1,19 @@
 import pytest
+import main
+
+
+def test_car_initialization():
+    car = main.Car("Toyota", "Camry", 2020, 20000)
+    assert car.make == "Toyota"
+    assert car.model == "Camry"
+    assert car.year == 2020
+    assert car.miles == 20000
+
+def test_drive_method():
+    car = main.Car("Toyota", "Camry", 2020, 20000)
+    car.drive(100)
+    assert car.miles == 20100
+
+def test_get_info_method():
+    car = main.Car("Toyota", "Camry", 2020, 20000)
+    assert car.get_info() == "Toyota Camry (2020) with 20000 miles"
